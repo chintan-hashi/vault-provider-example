1a2156dbe4a57b1cf11e5c75cca2e634a1e738fd
chintan-hashi <chintan@hashicorp.com>
2024-12-05 09:58:46 -0800

    adding new file

--- main.tf
+++ main.tf
@@ -13,6 +13,7 @@ provider "vault" {
 
     address = "https://vault-cluster-public-vault-0e71c1ed.94ac93b5.z1.hashicorp.cloud:8200"
     namespace = admin
+    token = "hvs.CAESICXEFMUVaT8xvKmO9DZ-Ww4wlYrLjq2wE5qzQaVIB1esGikKImh2cy44cFN2bktWWXRSa2xwcjlXUHhlNjhMYzUuRHM1WU4Q8M-3Aw"
 }
 
 data "vault_kv_secret_v2" "example" {
