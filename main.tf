0ecbd13797b1c519d2cd2019e58a4d317d0a1e11
chintan-hashi <chintan@hashicorp.com>
2024-12-05 09:55:53 -0800

    adding new file

--- main.tf
+++ main.tf
@@ -8,19 +8,15 @@ terraform {
   }
 }
 
-variable login_approle_role_id {
+variable token_val {
     type        = string
-    description = "Role ID for approle"
-}
-variable login_approle_secret_id {
-    type        = string
-    description = "Secret ID for approle"
+    description = "value of token"
 }
 
 provider "vault" {
 
     address = "https://vault-cluster-public-vault-0e71c1ed.94ac93b5.z1.hashicorp.cloud:8200"
-    token = "hvs.CAESICXEFMUVaT8xvKmO9DZ-Ww4wlYrLjq2wE5qzQaVIB1esGikKImh2cy44cFN2bktWWXRSa2xwcjlXUHhlNjhMYzUuRHM1WU4Q8M-3Aw"
+    token =    variable.token_val
     namespace = admin
 }
 
